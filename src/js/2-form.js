1560ab162db6c6149b316902633b9771d83a7c01
Irina <vyustirina@gmail.com>
2026-04-14 22:55:15 +0300

    add 2-form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,57 @@
-console.log('form');
+const formData = {
+   email: "",
+   message: ""
+}
+
+const KEY_FORM = "feedback-form-state";
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', handleSubmit);
+
+initForm();
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+ const email = formData.email.trim();
+  const message = formData.message.trim();
+
+  if (!email || !message) {
+    alert("Fill please all fields");
+ return;
+}
+console.log({email, message});
+
+  localStorage.removeItem(KEY_FORM);
+
+    formData.email = "";
+  formData.message = "";
+
+  form.reset();
+}
+
+function onFormInput(event) {
+
+  const {name, value} = event.target;
+  formData[name] = value;
+    
+  localStorage.setItem(KEY_FORM, JSON.stringify(formData));
+ }
+
+function initForm() {
+  const savedForm = localStorage.getItem(KEY_FORM);
+  
+  if (!savedForm) {
+    return;
+  }
+  
+  const parsedData = JSON.parse(savedForm);
+ formData.email = parsedData.email ?? "";
+  formData.message = parsedData.message ?? "";
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+  }
+
